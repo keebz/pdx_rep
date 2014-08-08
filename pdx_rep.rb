bf086fe1fa228062c09a218fb2b268bf4300fc50
Lee Kebler <student@epicodus.com>
2014-08-08 11:29:53 -0700

    let the user add the party affiliation and rep type to the new rep.

--- pdx_rep.rb
+++ pdx_rep.rb
@@ -27,9 +27,32 @@ end
 
 def add_rep
   puts "Please enter the name of your new representative:"
-  name = gets.chomp.to_s
+  name = gets.chomp.to_s.capitalize
   new_rep = Rep.new(name)
-  puts "Representative " + new_rep.name + " has been added"
+
+  puts "Representative " + new_rep.name + " has been added!"
+  puts "what party does representative " + new_rep.name + " belong to?"
+  party = gets.chomp.capitalize
+  new_rep.set_party(party)
+  puts new_rep.party + " " + new_rep.name + " has been updated!"
+
+  puts "Is " + new_rep.party + " " + new_rep.name + " a senator or congress person?"
+  loop do
+    puts "press 1 for 'Senator' or 2 for 'Congress person':"
+    selection = gets.chomp.to_i
+
+      if selection == 1
+        new_rep.set_rep_type('Senator')
+        break
+      elsif selection == 2
+        new_rep.set_rep_type('Congress person')
+        break
+      else
+        puts "not a valid input, try again!"
+      end
+  end
+  puts new_rep.type + ", " + new_rep.party + " " + new_rep.name + " has been updated!"
   main_menu
 end
+
 main_menu
