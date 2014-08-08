6719849ac0852da8b288929e63c98842dbd56911
Lee Kebler <student@epicodus.com>
2014-08-08 09:58:47 -0700

    allows for the rep type to be set

--- lib/rep.rb
+++ lib/rep.rb
@@ -1,5 +1,5 @@
 class Rep
-  attr_reader:name,:party
+  attr_reader:name,:party, :type
 
   def initialize(name)
     @name = name
@@ -7,5 +7,9 @@ class Rep
 
   def set_party(party)
     @party = party
+  end
+
+  def set_rep_type(type)
+    @type = type
   end
 end
--- spec/rep_spec.rb
+++ spec/rep_spec.rb
@@ -20,4 +20,17 @@ describe Rep do
     test_rep.set_party("democrate")
     expect(test_rep.party).to eq "democrate"
   end
+
+  it "defines the type or representitive a rep is" do
+    test_rep = Rep.new("bob")
+    test_rep.set_rep_type("senator")
+    expect(test_rep.type).to eq "senator"
+  end
+
+  it "creates an array of all the reps" do
+    test_rep = Rep.new("bob")
+    test_rep.set_party("democrate")
+    test_rep.set_rep_type("senator")
+    expect(Rep.all).to eq ["bob"]
+  end
 end
